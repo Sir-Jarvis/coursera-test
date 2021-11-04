c9bd904ab78bc896c73bd260f5253bb99fa885c0
Sir-Jarvis <sirjarvis@anything.com>
2021-11-04 15:26:35 +0000

    First Angular App

---
+++ app.js
@@ -0,0 +1,12 @@
+// An IIFE(Immadiately Invoked Function Expression)
+
+(function (){
+    'use strict';
+    let x = 'hello';
+
+    angular.module('MyFirstApp', [])
+
+    .controller('MyFirstController', function () {
+
+    });
+})();
